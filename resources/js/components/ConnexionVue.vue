70219729ebb25573b8d7f29a43f21facd0fd7c53
schneiderjacques <icrystal.fr@gmail.com>
2023-02-01 13:21:39 +0100

    connexion avancement first input

--- resources/js/components/ConnexionVue.vue
+++ resources/js/components/ConnexionVue.vue
@@ -9,7 +9,9 @@
                 <p class="text-2xl font-semibold text-primary-color mb-2">Welcome to Materio! 👋🏻</p>
                 <p class="mb-2 subtitle-1 text-subtitle-color">Please sign-in to your account and start the adventure</p>
             </div>
+            <div class="px-3 min-h-[36px] bg-amber-300">
 
+            </div>
         </div>
     </div>
 </template>
