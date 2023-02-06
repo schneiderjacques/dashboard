569761366f0672a5183c0bc67f21b6d6711ce02c
schneiderjacques <jacques.schneider@gmail.com>
2023-02-06 22:39:53 +0100

    Finalisation de la SideBarVue.vue

--- resources/js/components/SideBarVue.vue
+++ resources/js/components/SideBarVue.vue
@@ -75,7 +75,7 @@
                     data-v-6b6ad184=""
                     class="leading-[1.2] text-base flex-[1_1_100%] overflow-hidden overflow-ellipsis whitespace-nowrap text-current caret-current"
                 >
-                    Acount Settings
+                    Account Settings
                 </div>
             </router-link>
 
@@ -84,6 +84,9 @@
                 to="/"
                 :aria-expanded="isAccordionOpen"
                 class="flex items-center outline-none px-[16px] cursor-pointer text-primary-color caret-white h-[44px] mt-[0.375rem] min-h-[44px] pl-[22px] rounded-r-[32px] hover:bg-primary-color-opac"
+                :class="{
+                    'bg-gray-200': isAccordionOpen,
+                }"
             >
                 <div
                     class="inline-flex min-w-[24px] my-[12px] mr-[12px] text-current caret-current cursor-pointer"
@@ -134,17 +137,12 @@
             </router-link>
 
             <transition
-                enter-active-class="transition ease-out duration-1000"
-                enter-from-class="h-0"
-                enter-to-class="h-auto"
-                leave-active-class="transition ease-in duration-800"
-                leave-from-class="h-auto"
-                leave-to-class="h-0"
+                name="expand"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
             >
-                <div
-                    class="overflow-hidden"
-                    v-bind:style="{ height: isAccordionOpen ? 'auto' : '0' }"
-                >
+                <div v-if="isAccordionOpen">
                     <router-link
                         to="/"
                         class="flex items-center outline-none px-[16px] cursor-pointer text-primary-color caret-white h-[44px] mt-[0.375rem] min-h-[44px] pl-[22px] rounded-r-[32px] hover:bg-primary-color-opac"
@@ -238,8 +236,179 @@
                 </div>
             </transition>
 
-            <h2 class="mt-4px">Test</h2>
+            <div
+                class="mt-[18px] relative whitespace-nowrap items-center flex h-[48px] text-sm font-normal px-[16px] relative"
+            >
+                <span
+                    class="absolute left-0 leading-[.1em] w-full border-b-[1px] border-[rgba(94,86,105,.14)]"
+                >
+                    <span
+                        class="uppercase bg-[#f4f5fa] text-[.75rem] ml-[16px] px-[10px] text-subtitle-color"
+                        >user interface</span
+                    >
+                </span>
+            </div>
+
+            <router-link
+                to="/"
+                class="flex items-center outline-none px-[16px] cursor-pointer text-primary-color caret-white h-[44px] mt-[0.375rem] min-h-[44px] pl-[22px] rounded-r-[32px] hover:bg-primary-color-opac"
+            >
+                <div
+                    class="inline-flex min-w-[24px] my-[12px] mr-[12px] text-current caret-current cursor-pointer"
+                >
+                    <span
+                        data-v-6b6ad184=""
+                        aria-hidden="true"
+                        class="m-auto inline-flex text-[24px] justify-center relative select-none align-middle"
+                        ><svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            role="img"
+                            aria-hidden="true"
+                            class="w-[24px] h-[24px] fill-current"
+                        >
+                            <path
+                                d="M9,7H15V9H13V17H11V9H9V7M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M5,5V19H19V5H5Z"
+                            ></path></svg
+                    ></span>
+                </div>
+                <div
+                    data-v-6b6ad184=""
+                    class="leading-[1.2] text-base flex-[1_1_100%] overflow-hidden overflow-ellipsis whitespace-nowrap text-current caret-current"
+                >
+                    Typography
+                </div>
+            </router-link>
+
+            <router-link
+                to="/"
+                class="flex items-center outline-none px-[16px] cursor-pointer text-primary-color caret-white h-[44px] mt-[0.375rem] min-h-[44px] pl-[22px] rounded-r-[32px] hover:bg-primary-color-opac"
+            >
+                <div
+                    class="inline-flex min-w-[24px] my-[12px] mr-[12px] text-current caret-current cursor-pointer"
+                >
+                    <span
+                        data-v-6b6ad184=""
+                        aria-hidden="true"
+                        class="m-auto inline-flex text-[24px] justify-center relative select-none align-middle"
+                        ><svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            role="img"
+                            aria-hidden="true"
+                            class="w-[24px] h-[24px] fill-current"
+                        >
+                            <path
+                                d="M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,4.5C17,4.5 21.27,7.61 23,12C21.27,16.39 17,19.5 12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C15.76,17.5 19.17,15.36 20.82,12C19.17,8.64 15.76,6.5 12,6.5C8.24,6.5 4.83,8.64 3.18,12Z"
+                            ></path></svg
+                    ></span>
+                </div>
+                <div
+                    data-v-6b6ad184=""
+                    class="leading-[1.2] text-base flex-[1_1_100%] overflow-hidden overflow-ellipsis whitespace-nowrap text-current caret-current"
+                >
+                    Icons
+                </div>
+            </router-link>
+
+            <router-link
+                to="/"
+                class="flex items-center outline-none px-[16px] cursor-pointer text-primary-color caret-white h-[44px] mt-[0.375rem] min-h-[44px] pl-[22px] rounded-r-[32px] hover:bg-primary-color-opac"
+            >
+                <div
+                    class="inline-flex min-w-[24px] my-[12px] mr-[12px] text-current caret-current cursor-pointer"
+                >
+                    <span
+                        data-v-6b6ad184=""
+                        aria-hidden="true"
+                        class="m-auto inline-flex text-[24px] justify-center relative select-none align-middle"
+                        ><svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            role="img"
+                            aria-hidden="true"
+                            class="w-[24px] h-[24px] fill-current"
+                        >
+                            <path
+                                d="M20,8H4V6H20M20,18H4V12H20M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z"
+                            ></path></svg
+                    ></span>
+                </div>
+                <div
+                    data-v-6b6ad184=""
+                    class="leading-[1.2] text-base flex-[1_1_100%] overflow-hidden overflow-ellipsis whitespace-nowrap text-current caret-current"
+                >
+                    Cards
+                </div>
+            </router-link>
+
+            <router-link
+                to="/"
+                class="flex items-center outline-none px-[16px] cursor-pointer text-primary-color caret-white h-[44px] mt-[0.375rem] min-h-[44px] pl-[22px] rounded-r-[32px] hover:bg-primary-color-opac"
+            >
+                <div
+                    class="inline-flex min-w-[24px] my-[12px] mr-[12px] text-current caret-current cursor-pointer"
+                >
+                    <span
+                        data-v-6b6ad184=""
+                        aria-hidden="true"
+                        class="m-auto inline-flex text-[24px] justify-center relative select-none align-middle"
+                        ><svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            role="img"
+                            aria-hidden="true"
+                            class="w-[24px] h-[24px] fill-current"
+                        >
+                            <path
+                                d="M5,4H19A2,2 0 0,1 21,6V18A2,2 0 0,1 19,20H5A2,2 0 0,1 3,18V6A2,2 0 0,1 5,4M5,8V12H11V8H5M13,8V12H19V8H13M5,14V18H11V14H5M13,14V18H19V14H13Z"
+                            ></path></svg
+                    ></span>
+                </div>
+                <div
+                    data-v-6b6ad184=""
+                    class="leading-[1.2] text-base flex-[1_1_100%] overflow-hidden overflow-ellipsis whitespace-nowrap text-current caret-current"
+                >
+                    Tables
+                </div>
+            </router-link>
+
+            <router-link
+                to="/"
+                class="flex items-center outline-none px-[16px] cursor-pointer text-primary-color caret-white h-[44px] mt-[0.375rem] min-h-[44px] pl-[22px] rounded-r-[32px] hover:bg-primary-color-opac"
+            >
+                <div
+                    class="inline-flex min-w-[24px] my-[12px] mr-[12px] text-current caret-current cursor-pointer"
+                >
+                    <span
+                        data-v-6b6ad184=""
+                        aria-hidden="true"
+                        class="m-auto inline-flex text-[24px] justify-center relative select-none align-middle"
+                        ><svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            role="img"
+                            aria-hidden="true"
+                            class="w-[24px] h-[24px] fill-current"
+                        >
+                            <path
+                                d="M15 5H18L16.5 7L15 5M5 2H19C20.11 2 21 2.9 21 4V20C21 21.11 20.11 22 19 22H5C3.9 22 3 21.11 3 20V4C3 2.9 3.9 2 5 2M5 4V8H19V4H5M5 20H19V10H5V20M7 12H17V14H7V12M7 16H17V18H7V16Z"
+                            ></path></svg
+                    ></span>
+                </div>
+                <div
+                    data-v-6b6ad184=""
+                    class="leading-[1.2] text-base flex-[1_1_100%] overflow-hidden overflow-ellipsis whitespace-nowrap text-current caret-current"
+                >
+                    Form Layouts
+                </div>
+            </router-link>
         </div>
+        <a
+            class="absolute bottom-[13px] left-1/2 transform -translate-x-1/2 max-w-[230px] w-[560px] cursor-pointer"
+        >
+            <img src="../../../public/connexion/banner-light.png" />
+        </a>
     </div>
 </template>
 
@@ -251,7 +420,39 @@ export default {
             isAccordionOpen: false,
         };
     },
+    methods: {
+        enter(el) {
+            el.height = "auto";
+            const height = getComputedStyle(el).height;
+            el.style.height = 0;
+
+            getComputedStyle(el);
+
+            setTimeout(() => {
+                el.style.height = height;
+            });
+        },
+        afterEnter(el) {
+            el.style.height = "auto";
+        },
+        leave(el) {
+            el.style.height = getComputedStyle(el).height;
+            getComputedStyle(el);
+            setTimeout(() => {
+                el.style.height = 0;
+            });
+        },
+    },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.expand-leave-active {
+    transition: height 0.5s ease-in-out;
+    overflow: hidden;
+}
+.expand-enter-active {
+    transition: height 0.5s ease-in-out;
+    overflow: hidden;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue"
-  ],
-  theme: {
-    extend: {
-        boxShadow: {
-          'card' : '0 2px 10px 0 rgb(94 86 105 / 10%)',
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            transitionProperty: {
+                height: "height",
+            },
+            boxShadow: {
+                card: "0 2px 10px 0 rgb(94 86 105 / 10%)",
+            },
+            colors: {
+                "primary-background": "#f4f5fa;",
+                "primary-color": "rgba(94,86,105,.87)",
+                "primary-color-opac": "rgba(94,86,105,.04)",
+                "anchor-base": "#9155fd;",
+                "subtitle-color": "rgba(94,86,105,.68)",
+                "primary-base": "#9155fd",
+                "theme-primary": "rgb(145,85,253)",
+                "theme-on-surface": "rgb(58,53,65)",
+            },
+            spacing: {
+                100: "28rem",
+            },
         },
-        colors: {
-            'primary-background': '#f4f5fa;',
-            'primary-color': 'rgba(94,86,105,.87)',
-            'primary-color-opac': 'rgba(94,86,105,.04)',
-            'anchor-base': '#9155fd;',
-            'subtitle-color': 'rgba(94,86,105,.68)',
-            'primary-base': '#9155fd',
-            'theme-primary': 'rgb(145,85,253)',
-            'theme-on-surface': 'rgb(58,53,65)',
-        },
-        spacing: {
-            '100': '28rem',
-        }
     },
-  },
-  plugins: [
-      //...
-      require('@tailwindcss/forms'),
-  ],
-}
+    plugins: [
+        //...
+        require("@tailwindcss/forms"),
+    ],
+};
